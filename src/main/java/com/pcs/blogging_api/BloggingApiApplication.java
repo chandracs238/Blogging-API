6821b6c0a9911614ad211e1160f8541ff128bf88
chandracs238 <chandhu7013@gmail.com>
2024-11-16 21:58:05 +0530

    Initial commit

---
+++ src/main/java/com/pcs/blogging_api/BloggingApiApplication.java
@@ -0,0 +1,13 @@
+package com.pcs.blogging_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApiApplication.class, args);
+	}
+
+}
